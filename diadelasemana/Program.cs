e2ce12746735cbda5c07b6b780d25eca23567768
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 16:18:44 -0300

    Add files via upload

---
+++ diadelasemana/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Dias_de_la_Semana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string linea;
+            Console.WriteLine();
+            Console.WriteLine("DIAS DE LA SEMANA");
+            Console.WriteLine();
+            Console.Write("Ingrese un numero del 1 al 7 :");
+            linea = Console.ReadLine();
+            num = int.Parse(linea);
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine();
+                    Console.WriteLine("El numero que ingreso corresponde al dia DOMINGO");
+ break;
+                case 2:
+                    Console.WriteLine();
+                    Console.WriteLine("El numero que ingreso corresponde al dia LUNES");
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    Console.WriteLine("El numero que ingreso corresponde al dia MARTES");
+                    break;
+                case 4:
+                    Console.WriteLine();
+                    Console.WriteLine("El numero que ingreso corresponde al diaMIERCOLES");
+ break;
+                case 5:
+                    Console.WriteLine();
+                    Console.WriteLine("El numero que ingreso corresponde al dia JUEVES");
+                    break;
+                case 6:
+                    Console.WriteLine();
+                    Console.WriteLine("El numero que ingreso corresponde al dia VIERNES");
+ break;
+                case 7:
+                    Console.WriteLine();
+                    Console.WriteLine("El numero que ingreso corresponde al dia SABADO");
+                    break;
+                default:
+                    Console.WriteLine();
+                    Console.WriteLine("El numero que ingreso esta fuera de rango");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
